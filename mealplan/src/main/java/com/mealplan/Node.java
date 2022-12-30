0a90a0039f571ababd212cefd4edc7d66b28e482
shea6utter <an6sty@gmail.com>
2022-12-30 18:00:02 +0800

    Initial commit

---
+++ mealplan/src/main/java/com/mealplan/Node.java
@@ -0,0 +1,41 @@
+package com.mealplan;
+
+enum Weekday {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
+
+enum TimeOfDay {
+    MORNING,
+    AFTERNOON,
+    EVENING
+}
+
+public class Node {
+
+    private String meal;
+    private String day;
+    private String time;
+    
+    Node() {
+        meal = "";
+        day = "";
+        time = "";
+    }
+    
+    Node(String strMeal, String strDay, String strTime) {
+        meal = strMeal;
+        day = strDay;
+        time = strTime;
+    }
+
+    public void setData(String strMeal, String strDay, String strTime) {
+            
+    }
+    
+}
